c29e3080c178451b73cb5d8fe04726b8acfe10a4
niranjan <106364901+niranjan1424@users.noreply.github.com>
2023-05-04 11:41:32 +0530

    Create 5.dataframe.r

---
+++ 5.dataframe.r
@@ -0,0 +1,10 @@
+exam = data.frame(
+  name = c('Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'),
+  score = c(12.5, 9, 16.5, 12, 9, 20, 14.5, 13.5, 8, 19),
+  attempts = c(1, 3, 2, 3, 2, 3, 1, 1, 2, 1),
+  qualify = c('yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes')
+)
+print(exam)
+print("Extract 3rd and 5th rows with 1st and 3rd columns :")
+result =  exam[c(3,5),c(1,3)]
+print(result)
